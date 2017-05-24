826215f4ba7f33f33a67520bdee35adb902fe08d
poonamAppirio <poonam.singh@appirio.com>
2017-05-25 03:13:10 +0530

    Update auto1.js

--- 008-Jquery-autocomplete/js/auto1.js
+++ 008-Jquery-autocomplete/js/auto1.js
@@ -1,4 +1,4 @@
-/*function initialize() {
+function initialize() {
       var options = {
   types: ['(regions)'],
   componentRestrictions: {country: "us"}
@@ -7,71 +7,4 @@
       var autocomplete = new google.maps.places.Autocomplete(input,options);
    }
 
-   google.maps.event.addDomListener(window, 'load', initialize);*/
-
- $( function() {
-    var availableTags = [
-        "Alabama",
-        "Alaska",
-        "American Samoa",
-        "Arizona",
-        "Arkansas",
-        "California",
-        "Colorado",
-        "Connecticut",
-        "Delaware",
-        "District Of Columbia",
-        "Federated States Of Micronesia",
-        "Florida",
-        "Georgia",
-        "Guam",
-        "Hawaii",
-        "Idaho",
-        "Illinois",
-        "Indiana",
-        "Iowa",
-        "Kansas",
-        "Kentucky",
-        "Louisiana",
-        "Maine",
-        "Marshall Islands",
-        "Maryland",
-        "Massachusetts",
-        "Michigan",
-        "Minnesota",
-        "Mississippi",
-        "Missouri",
-        "Montana",
-        "Nebraska",
-        "Nevada",
-        "New Hampshire",
-        "New Jersey",
-        "New Mexico",
-        "New York",
-        "North Carolina",
-        "North Dakota",
-        "Northern Mariana Islands",
-        "Ohio",
-        "Oklahoma",
-        "Oregon",
-        "Palau",
-        "Pennsylvania",
-        "Puerto Rico",
-        "Rhode Island",
-        "South Carolina",
-        "South Dakota",
-        "Tennessee",
-        "Texas",
-        "Utah",
-        "Vermont",
-        "Virgin Islands",
-        "Virginia",
-        "Washington",
-        "West Virginia",
-        "Wisconsin",
-        "Wyoming"
-    ];
-    $( "#tags" ).autocomplete({
-      source: availableTags
-    });
-  } );
+   google.maps.event.addDomListener(window, 'load', initialize);
